93f47bee9a6563ea79675ca21ad01fa38fc944fd
kdobro92 <kdobro92@gmail.com>
2022-12-12 22:43:34 +0900

    [추가] use map, hashmap

---
+++ collections/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kimdohyung
+ *
+ */
+module collections {
+}
--- objectOrientedProgramming2/src/com/in28minutes/oops/level2/RectangleRunner.java
+++ objectOrientedProgramming2/src/com/in28minutes/oops/level2/RectangleRunner.java
@@ -6,6 +6,6 @@ public class RectangleRunner {
 		Rectangle rectangle = new Rectangle(12, 23);
 		System.out.println(rectangle);
 		rectangle.setWidth(25);
-		System.out.println(rectangle);
+		System.out.println(rectangle); 
 	}
 }
