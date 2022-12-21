bd2edfe954f0fab73f8fe5884b8d5c96d3319ea5
kdobro92 <kdobro92@gmail.com>
2022-12-21 22:57:27 +0900

    spring boot

---
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringboot.courses.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.learnspringboot.courses.bean.Course;
+
+@RestController
+public class CourseController {
+
+	//http://locatlhost:8080/courses
+	@GetMapping("/courses")
+	public List<Course> getAllCourses() {
+		return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"));
+	}
+}
