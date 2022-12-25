95aef4585d9dec756eeca156e64bf0ec6f2e0fdc
kdobro92 <kdobro92@gmail.com>
2022-12-25 19:06:45 +0900

    spring boot

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/LearnSpringBootApplication.java
@@ -1,9 +1,19 @@
 package com.in28minutes.learnspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
 
 @SpringBootApplication
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
+
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
