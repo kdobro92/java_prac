a45070a983216b4165d6eed92a74ab18addc933a
kdobro92 <kdobro92@gmail.com>
2022-12-28 22:44:23 +0900

    day15

---
+++ generics/src/com/in28minutes/generics/MyCustomList.java
@@ -0,0 +1,19 @@
+package com.in28minutes.generics;
+
+import java.util.ArrayList;
+
+public class MyCustomList<T> {
+	ArrayList<T> list =  new ArrayList<>();
+	
+	public void addElement(T element) {
+		list.add(element);
+	}
+	
+	public void removeElement(T element) {
+		list.remove(element);
+	}
+	
+	public String toString() {
+		return list.toString();
+	}
+}
