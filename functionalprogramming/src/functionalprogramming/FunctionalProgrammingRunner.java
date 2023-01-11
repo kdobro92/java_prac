59c200765d47626ec81071019d58d5a07160b586
kdobro92 <kdobro92@gmail.com>
2023-01-11 20:39:34 +0900

    day23

--- functionalprogramming/src/functionalprogramming/FunctionalProgrammingRunner.java
+++ functionalprogramming/src/functionalprogramming/FunctionalProgrammingRunner.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class FunctionalProgrammingRunner {
 
 	public static void main(String[] args) {
-//		List<String> list = List.of("Apple");
+		List<String> list = List.of("Apple");
 
-//		printBasic(list);
+		printBasic(list);
 	}
 
 	private static void printBasic(List<String> list) {
